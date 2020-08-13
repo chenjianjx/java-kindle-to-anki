1fdf09b20e811b08bbd0161f4fb68ded22df4c71
chenjianjx@gmail.com <chenjianjx@gmail.com>
2020-08-13 15:36:40 +1000

    ready to go public

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # java-kindle-to-anki
 A java program that converts kindle highlights html to anki csv file for importing
+ 
+
+# How to run
+Note:  minimum jdk (not just jre) version: 1.8
+
+```bash
+mvn clean package 
+unzip target/java-kindle-to-anki*jarset.zip -d /path/to/your/dir
+cd /path/to/your/dir/java-kindle-to-anki*
+./k2a.sh #Just to see the arguments
+./k2a.sh /path/to/your/kindleHightlights.html  
+# or k2a.bat for windows
+``` 
+
+After that you will get a csv file.  Import it into your Anki application. 
+You will see new cards created:  
+* the front of a card is a piece ofe highlight
+* the backend is empty
--- src/main/java/com/chenjianjx/k2a/ui/Kindle2AnkiMain.java
+++ src/main/java/com/chenjianjx/k2a/ui/Kindle2AnkiMain.java
@@ -14,27 +14,34 @@ public class Kindle2AnkiMain {
 
     public static void main(String[] args) throws IOException {
         if (args == null || args.length == 0) {
-            System.out.println("Params:  kindleHighlightsHtmlFile [targetDir]");
+            System.out.println("Params:  kindleHighlightsHtmlFile [targetFile]");
             return;
         }
 
         List<String> argList = new ArrayList<String>(Arrays.asList(args));
 
-        File htmlFile = new File( argList.get(0));
+        File htmlFile = new File(argList.get(0)).getAbsoluteFile();
 
-        File outputDir = htmlFile.getParentFile();
-        if (argList.size() > 1) {
-            outputDir = new File(argList.get(1));
-            outputDir.mkdirs();
+
+        if (!htmlFile.exists()) {
+            System.err.println(htmlFile + " doesn't exist");
+            System.exit(-1);
         }
 
-        File targetFile = new File(outputDir, FilenameUtils.getBaseName(htmlFile.getName()) + ".csv");
+        File targetFile;
+
+        if (argList.size() > 1) {
+            targetFile = new File(argList.get(1)).getAbsoluteFile();
+            targetFile.getParentFile().mkdirs();
+        } else {
+            targetFile = new File(htmlFile.getParentFile(), FilenameUtils.getBaseName(htmlFile.getName()) + ".csv");
+        }
 
 
         Kindle2Anki k2a = new Kindle2Anki();
 
         String kindleHighlights = FileUtils.readFileToString(htmlFile, "UTF8");
-        String ankiCsv =  k2a.convert(kindleHighlights);
+        String ankiCsv = k2a.convert(kindleHighlights);
 
         FileUtils.writeStringToFile(targetFile, ankiCsv, "UTF8");
 
